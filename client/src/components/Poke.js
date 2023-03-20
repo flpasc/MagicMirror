85727ba043915d8f6f211ef5f60017974a8d15df
Florian <florianpaulschoene@gmail.com>
2023-03-20 11:15:15 +0100

    integrated pokemon api calls and mongo db

--- client/src/components/Poke.js
+++ client/src/components/Poke.js
@@ -1,6 +1,10 @@
 import '../assets/styles/Poke.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Poke() {
+	const [pokemonOfDay, setpokemonOfDay] = useState({})
+
+	useEffect
+
 	return <div className='poke--container'>Poke</div>
 }
