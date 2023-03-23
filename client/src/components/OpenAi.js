6b77d4952c1960d3ba9bc0280ff257cc8ee6c4cc
Florian <florianpaulschoene@gmail.com>
2023-03-23 13:39:52 +0100

    opanai refresh interval

--- client/src/components/OpenAi.js
+++ client/src/components/OpenAi.js
@@ -3,12 +3,24 @@ import '../assets/styles/Openai.css'
 
 export default function OpenAi() {
 	const [aiText, setAiText] = useState('')
+	const [initialze, setInitialize] = useState(false)
 
 	useEffect(() => {
-		fetch('/api/v1/openai')
-			.then((res) => res.json())
-			.then((data) => setAiText(data.text))
-			.catch((error) => console.log('Error fetching aitext from server', error))
+		if (!initialze) {
+			fetch('/api/v1/openai')
+				.then((res) => res.json())
+				.then((data) => setAiText(data.text))
+				.catch((error) => console.log('Error fetching aitext from server', error))
+			setInitialize(true)
+		}
+
+		const interval = setInterval(() => {
+			fetch('/api/v1/openai')
+				.then((res) => res.json())
+				.then((data) => setAiText(data.text))
+				.catch((error) => console.log('Error fetching aitext from server', error))
+		}, 1000 * 60 * 60 * 3)
+		return () => clearInterval(interval)
 	}, [])
 
 	return (
