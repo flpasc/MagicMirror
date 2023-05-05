910738e6afcc07485c317dd5a22b345a0ea66245
flpasc <111666367+flpasc@users.noreply.github.com>
2023-05-05 22:30:30 +0200

    Update Clock.js

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -5,17 +5,15 @@ import Moment from 'react-moment'
 export default function Clock() {
 	const [currentTime, setCurrentTime] = useState('')
 
-	useEffect(() => {
-		let interval = null
-		interval = setInterval(() => {
-			fetch('/api/v1/clock')
-				.then((res) => res.json())
-				.then((data) => setCurrentTime(data))
-				.catch((error) => console.log(error))
-		}, 1000)
+  useEffect(() => {
+    let interval = null
+    interval = setInterval(() => {
+      const now = new Date()
+      setCurrentTime(now.toLocaleTimeString())
+    }, 1000)
 
-		return () => clearInterval(interval)
-	}, [])
+    return () => clearInterval(interval)
+  }, [])
 	return (
 		<div className='clock--container'>
 			<Moment className='clock--houres' format='HH'>
