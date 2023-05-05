ceffa38cc1a8a35a85c48e7f1a5b6b36d27e9ccf
flpasc <111666367+flpasc@users.noreply.github.com>
2023-05-05 20:55:19 +0200

    Update scheduler.js
    once a week

--- server/services/scheduler.js
+++ server/services/scheduler.js
@@ -2,7 +2,7 @@ const cron = require('node-cron')
 const { fetchPokemonData, storePokemonData, updatePokemonData } = require('../services/pokemon')
 let init = false
 
-const pokeSchedule = cron.schedule('*/10 * * * * *', async () => {
+const pokeSchedule = cron.schedule('0 3 * * 1', async () => {
 	let weeklyPokemon = await fetchPokemonData()
 	await updatePokemonData()
 	await storePokemonData(weeklyPokemon)
