88ba9080107e822b57ff1aa756454dd87818c99e
Florian <florianpaulschoene@gmail.com>
2023-05-09 16:11:28 +0200

    remove openAI

--- client/src/components/OpenAi.js
+++ client/src/components/OpenAi.js
@@ -5,24 +5,24 @@ export default function OpenAi() {
 	const [aiText, setAiText] = useState('')
 	const [initialze, setInitialize] = useState(false)
 
-	useEffect(() => {
-		if (!initialze) {
-			const fetchURL = `http://localhost:6600/api/v1/openai`
-			fetch(fetchURL)
-				.then((res) => res.json())
-				.then((data) => setAiText(data.text))
-				.catch((error) => console.log('Error fetching aitext from server', error))
-			setInitialize(true)
-		}
+	// useEffect(() => {
+	// 	if (!initialze) {
+	// 		const fetchURL = `http://localhost:6600/api/v1/openai`
+	// 		fetch(fetchURL)
+	// 			.then((res) => res.json())
+	// 			.then((data) => setAiText(data.text))
+	// 			.catch((error) => console.log('Error fetching aitext from server', error))
+	// 		setInitialize(true)
+	// 	}
 
-		const interval = setInterval(() => {
-			fetch('/api/v1/openai')
-				.then((res) => res.json())
-				.then((data) => setAiText(data.text))
-				.catch((error) => console.log('Error fetching aitext from server', error))
-		}, 1000 * 60 * 60 * 3)
-		return () => clearInterval(interval)
-	}, [])
+	// 	const interval = setInterval(() => {
+	// 		fetch('http://localhost:6600/api/v1/openai')
+	// 			.then((res) => res.json())
+	// 			.then((data) => setAiText(data.text))
+	// 			.catch((error) => console.log('Error fetching aitext from server', error))
+	// 	}, 1000 * 60 * 60 * 3)
+	// 	return () => clearInterval(interval)
+	// }, [])
 
 	return (
 		<div className='openai--container'>
--- server/services/openai.js
+++ server/services/openai.js
@@ -1,27 +1,27 @@
-const axios = require('axios')
-const { Configuration, OpenAIApi } = require('openai')
+// const axios = require('axios')
+// const { Configuration, OpenAIApi } = require('openai')
 
-//Enter openai question here
-const openaiPrompt =
-	'give me a short random fact about a animal that is totaly not true but could be true'
+// //Enter openai question here
+// const openaiPrompt =
+// 	'give me a short random fact about a animal that is totaly not true but could be true'
 
-const configuration = new Configuration({
-	apiKey: process.env.OPENAI_API_KEY,
-})
-const openai = new OpenAIApi(configuration)
+// const configuration = new Configuration({
+// 	apiKey: process.env.OPENAI_API_KEY,
+// })
+// const openai = new OpenAIApi(configuration)
 
-async function fetchAiText() {
-	try {
-		const newText = await openai.createCompletion({
-			model: 'text-davinci-003',
-			temperature: 1.2,
-			prompt: openaiPrompt,
-			max_tokens: 1000,
-		})
-		return newText
-	} catch (error) {
-		console.log('Error fetching ai text', error)
-	}
-}
+// async function fetchAiText() {
+// 	try {
+// 		const newText = await openai.createCompletion({
+// 			model: 'text-davinci-003',
+// 			temperature: 1.2,
+// 			prompt: openaiPrompt,
+// 			max_tokens: 1000,
+// 		})
+// 		return newText
+// 	} catch (error) {
+// 		console.log('Error fetching ai text', error)
+// 	}
+// }
 
-module.exports = { fetchAiText }
+// module.exports = { fetchAiText }
